269afd455cad7fd5624219262046855c6ee10a4a
zcrome <soportezcrome@gmail.com>
2018-06-08 15:33:48 -0500

    Initial Commit

---
+++ IOSFormValidator/IOSFormValidator.h
@@ -0,0 +1,19 @@
+//
+//  IOSFormValidator.h
+//  IOSFormValidator
+//
+//  Created by Melanie on 6/8/18.
+//  Copyright © 2018 Eduardo Palomino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IOSFormValidator.
+FOUNDATION_EXPORT double IOSFormValidatorVersionNumber;
+
+//! Project version string for IOSFormValidator.
+FOUNDATION_EXPORT const unsigned char IOSFormValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOSFormValidator/PublicHeader.h>
+
+
